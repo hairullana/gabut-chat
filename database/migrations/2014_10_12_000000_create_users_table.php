b750839ada7f9fb1b6244a454ef01fe565c8b3ec
Hairul Lana <hairullana99@gmail.com>
2022-01-20 18:24:42 +0800

    update form username and migrate

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,12 +15,12 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('name')->nullable();
             $table->string('username')->unique();
-            $table->string('email');
+            $table->string('email')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->rememberToken();
+            $table->rememberToken()->nullable();
             $table->timestamps();
         });
     }
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,14 +4,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Laravel Chat</title>
+    <title>Gaboet Chat</title>
     <link rel="stylesheet" href="/css/app.css">
 </head>
 <body>
     <div class="app">
         <header>
-            <h1>Let's Talk</h1>
-            <input type="text" name="username" id="username" placeholder="Username">
+            <h1>Gaboet</h1>
+            <input type="text" name="username" id="username" value="Username: {{ Auth::user()->username }}" disabled>
         </header>
 
         <div id="messages"></div>
