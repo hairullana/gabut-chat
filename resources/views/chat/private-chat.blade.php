bebf378a6f6dd5fc313f9c643d3a009c37f20481
Hairul Lana <hairullana99@gmail.com>
2022-01-26 17:04:36 +0800

    dynamic time message

--- resources/views/chat/private-chat.blade.php
+++ resources/views/chat/private-chat.blade.php
@@ -40,22 +40,15 @@
                           <small>Last seen: x hours ago</small>
                       </div>
                   </div>
-                  {{-- <div class="col-lg-6 hidden-sm text-right">
-                      <a href="javascript:void(0);" class="btn btn-outline-secondary"><i class="fa fa-camera"></i></a>
-                      <a href="javascript:void(0);" class="btn btn-outline-primary"><i class="fa fa-image"></i></a>
-                      <a href="javascript:void(0);" class="btn btn-outline-info"><i class="fa fa-cogs"></i></a>
-                      <a href="javascript:void(0);" class="btn btn-outline-warning"><i class="fa fa-question"></i></a>
-                  </div> --}}
               </div>
           </div>
           <div class="chat-history" id="chat-history">
               <ul id="privateMessage">
-                {{-- {{ dd($messages->messages->message) }} --}}
                 @foreach ($messages as $message)
                   @if ($message->user_id == Auth::user()->id)
                     <li class="clearfix">
                       <div class="message-data text-right">
-                          <span class="message-data-time">10:10 AM, Today</span>
+                          <span class="message-data-time">{{ date_format($message->created_at, "H:i (d M Y)") }}</span>
                           <img src="https://bootdey.com/img/Content/avatar/avatar7.png" alt="avatar">
                       </div>
                       <div class="message other-message float-right">
@@ -65,7 +58,7 @@
                   @else
                     <li class="clearfix">
                       <div class="message-data">
-                          <span class="message-data-time">10:12 AM, Today</span>
+                          <span class="message-data-time">{{ date_format($message->created_at, "H:i (d M Y)") }}</span>
                       </div>
                       <div class="message my-message">
                         {{ $message->message }}
@@ -73,19 +66,6 @@
                     </li>
                   @endif
                 @endforeach
-                  {{-- 
-                  <li class="clearfix">
-                      <div class="message-data">
-                          <span class="message-data-time">10:12 AM, Today</span>
-                      </div>
-                      <div class="message my-message">Are we meeting today?</div>                                    
-                  </li>                               
-                  <li class="clearfix">
-                      <div class="message-data">
-                          <span class="message-data-time">10:15 AM, Today</span>
-                      </div>
-                      <div class="message my-message">Project has been already finished and I have results to show you.</div>
-                  </li> --}}
               </ul>
           </div>
           <div class="chatMessageClearfix">
